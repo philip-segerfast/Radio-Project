b776cf8dff867233af4c3bc8f07e17cc7a146829
Philip <psegerfast@gmail.com>
2021-03-19 10:05:32 +0100

    Små ändringar

--- src/main/java/com/grupp4/radioproject/controllers/ProgramController.java
+++ src/main/java/com/grupp4/radioproject/controllers/ProgramController.java
@@ -3,7 +3,6 @@ package com.grupp4.radioproject.controllers;
 import com.grupp4.radioproject.entities.Program;
 import com.grupp4.radioproject.services.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,7 @@ public class ProgramController {
 
     @GetMapping("/rest/programs/{page}")
     public List<Program> getAllPrograms(@PathVariable int page) {
-        var programs = programService.getAllPrograms(page);
+        var programs = programService.getAllProgramsAtPage(page);
         System.out.println(programs);
         return programs;
     }
