0eb7d8769febe2f12bea6c2457111f59e5dcb8da
Philip <psegerfast@gmail.com>
2021-03-19 15:18:16 +0100

    Gjorde så man kan hämta ut alla kanaler + mer (läs description)
    * Lade till klassen ChannelController med en GetMapping för "/rest/channels".
    * Tog bort alla description-filer för de används inte.
    * Lade även till fältet "tagline" i Channel-entitetsklassen. Det är en beskrivning för en kanal och kan vara bra att ha med.

---
+++ src/main/java/com/grupp4/radioproject/controllers/ChannelController.java
@@ -0,0 +1,22 @@
+package com.grupp4.radioproject.controllers;
+
+import com.grupp4.radioproject.entities.Channel;
+import com.grupp4.radioproject.services.ChannelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ChannelController {
+
+    @Autowired
+    private ChannelService channelService;
+
+    @GetMapping("/rest/channels")
+    public List<Channel> getAllChannels() {
+        return channelService.getAllChannels();
+    }
+
+}
