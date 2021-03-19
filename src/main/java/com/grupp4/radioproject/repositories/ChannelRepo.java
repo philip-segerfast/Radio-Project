b25c273f2e2138cfec88521aa6a9979fe6bb3623
Elias Rundqvist <rundqvistelias@gmail.com>
2021-03-19 14:20:29 +0100

    Ny branch

--- src/main/java/com/grupp4/radioproject/repositories/ChannelRepo.java
+++ src/main/java/com/grupp4/radioproject/repositories/ChannelRepo.java
@@ -1,7 +1,10 @@
 package com.grupp4.radioproject.repositories;
 
+import com.grupp4.radioproject.entities.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChannelRepo {
+public interface ChannelRepo extends JpaRepository<Channel, Long> {
+    Channel findByChannel(String channel);
 }
--- src/main/java/com/grupp4/radioproject/repositories/ProgramRepo.java
+++ src/main/java/com/grupp4/radioproject/repositories/ProgramRepo.java
@@ -1,7 +1,10 @@
 package com.grupp4.radioproject.repositories;
 
+import com.grupp4.radioproject.entities.Program;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProgramRepo {
+public interface ProgramRepo extends JpaRepository<Program, Long> {
+    Program findByProgram(String program_id);
 }
