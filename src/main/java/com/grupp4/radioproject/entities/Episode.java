3c10c4bc89d088de199874825a08a8501d0b7421
Elias Rundqvist <rundqvistelias@gmail.com>
2021-03-22 09:21:10 +0100

    Getprogrambychannel

--- src/main/java/com/grupp4/radioproject/entities/Episode.java
+++ src/main/java/com/grupp4/radioproject/entities/Episode.java
@@ -8,7 +8,28 @@ public class Episode {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long episodeId;
+    private String name;
 
     private Episode() {}
 
+    public Episode(long id, String name) {
+        this.episodeId = id;
+        this.name = name;
+    }
+
+    public long getEpisodeId() {
+        return episodeId;
+    }
+
+    public void setEpisodeId(long episodeId) {
+        this.episodeId = episodeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Episode{" +
+                "episodeId=" + episodeId +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
