671ca71bf5625862ccb8075f2c716cea4c0f4745
Philip <psegerfast@gmail.com>
2021-03-24 10:57:36 +0100

    Finalized

--- src/main/java/com/grupp4/radioproject/utils/PrintUtils.java
+++ src/main/java/com/grupp4/radioproject/utils/PrintUtils.java
@@ -4,19 +4,12 @@ import static com.grupp4.radioproject.utils.ConsoleColor.*;
 
 public class PrintUtils {
 
-    private static final ConsoleColor ERROR_COLOR = WHITE;
+    private static final ConsoleColor ERROR_COLOR = GRAY;
     private static final ConsoleColor ERROR_BG_COLOR = RED_BG;
-    private static final ConsoleColor INFO_COLOR = GREEN;
-    private static final ConsoleColor INFO_BG_COLOR = WHITE_BG;
+    private static final ConsoleColor INFO_COLOR = GRAY;
+    private static final ConsoleColor INFO_BG_COLOR = GREEN_BG;
     private static final ConsoleColor DEBUG_COLOR = GRAY;
-    private static final ConsoleColor DEBUG_BG_COLOR = YELLOW_BG;
-
-    static {
-        printDebug("This is a debug message!");
-        printError("This is an error message!");
-        printInfo("This is  an info message!");
-    }
-
+    private static final ConsoleColor DEBUG_BG_COLOR = BRIGHT_BLUE_BG;
 
     /**
      * <p>
@@ -36,19 +29,23 @@ public class PrintUtils {
      * </p>
      */
     public static void printColoredLine(String str, ConsoleColor color) {
-        System.out.println(color + str + RESET);
+        doPrint(color + str);
     }
 
     public static void printError(String str) {
-        System.out.println("" + ERROR_BG_COLOR + ERROR_COLOR + " ERROR " + RESET + RED + " " + str + RESET);
+        doPrint("" + ERROR_BG_COLOR + ERROR_COLOR + "[ERROR]" + RESET + RED + " " + str);
     }
 
     public static void printInfo(String str) {
-        System.out.println("" + GREEN_BG + WHITE + " INFO " + RESET + GREEN + " " + str);
+        doPrint("" + INFO_BG_COLOR + INFO_COLOR + "[INFO]" + RESET + GREEN + " " + str);
     }
 
     public static void printDebug(String str) {
-        System.out.println("" + DEBUG_BG_COLOR + DEBUG_COLOR + " DEBUG " + RESET + YELLOW + " "  + str);
+        doPrint("" + DEBUG_COLOR + DEBUG_BG_COLOR + "[DEBUG]" + RESET + BLUE + " "  + str);
+    }
+
+    private static void doPrint(String str) {
+        System.out.println(str + RESET);
     }
 
 }
